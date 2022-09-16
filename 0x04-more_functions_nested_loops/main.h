42f65e92e168b9a4f7740602f733b3e24a7a3375
EDO-code <dorathygift0@gmail.com>
2022-09-16 05:59:36 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,123 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to be put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+ * _isupper - determines uppercase characters
+ * @c: character to determine
+ * Return: true or false
+ */
+
+int _isupper(int c);
+
+/**
+ * _isdigit - checks for digits
+ * @c: character to check
+ *
+ * Return: returns boolean
+ */
+
+int _isdigit(int c);
+
+/**
+ * mul - multiplies inputs
+ * @a: first integer
+ * @b: second integer
+ * Return: gives output
+ */
+
+int mul(int a, int b);
+
+/**
+ * print_numbers - prints numbers
+ *
+ */
+
+void print_numbers(void);
+
+/**
+ * print_most_numbers - prints numbers
+ *
+ */
+
+void print_most_numbers(void);
+
+/**
+ * more_numbers - prints more numbers
+ *
+ */
+
+void more_numbers(void);
+
+/**
+ * print_line - prints a line
+ *
+ * @n: integer to print
+ */
+
+void print_line(int n);
+
+/**
+ * print_diagonal - prints diagonal
+ * @n: integer to see how many lines
+ */
+
+void print_diagonal(int n);
+
+/**
+ * print_square - prints a square
+ *
+ * @size: prints the size
+ */
+
+void print_square(int size);
+
+/**
+ * print_triangle - prints triangle
+ * @size: size of triangle
+ */
+
+void print_triangle(int size);
+
+/**
+ * print_number - print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
+ */
+
+void print_number(int n);
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
