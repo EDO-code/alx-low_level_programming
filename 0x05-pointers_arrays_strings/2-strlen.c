f7a3c1807b6a8ede89ce3a8bef8f852754200f3f
EDO-code <dorathygift0@gmail.com>
2022-09-20 09:20:38 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: string
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	/* c is used as counter */
+
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		continue;
+	}
+	return (c);
+}
+
