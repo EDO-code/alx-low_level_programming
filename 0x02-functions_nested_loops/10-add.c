a7dcd4fa32d30440b6030cfc11ce6016fe43fc03
EDO-code <dorathygift0@gmail.com>
2022-09-21 14:17:11 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - checks if a character is lowecased or not
+ * @a: prop1
+ * @b: prop2
+ *
+ * Adds two numbers
+ *
+ * Return: returns sum a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
