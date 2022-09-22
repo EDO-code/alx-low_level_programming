f025681888ced1315092cfaae230b26694204c1e
EDO-code <dorathygift0@gmail.com>
2022-09-22 16:24:50 -0700

    add

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main -  main program
+*
+* Description: prints lower case letters a - z
+*
+* Return: 0 indicates success
+*/
+int main(void)
+{
+	int rtc;
+
+
+	for (rtc = 0; rtc < 10; rtc++)
+	{
+		putchar(rtc + '0');
+		if (rtc < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
