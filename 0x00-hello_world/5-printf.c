19f1e217433c1032c87a5090c9811643b31f0159
EDO-code <dorathygift0@gmail.com>
2022-09-23 14:50:58 -0700

    add

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char txt[] = "with proper grammar%c but the outcome is a piece of art%c\n";
+
+	printf(txt, ',', ',');
+	return (0);
+}
